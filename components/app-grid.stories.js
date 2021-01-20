e3f8c71b194c3be201b54021bbadd7e42d2ce7fe
Jeremias Erbs <jerry@badabam.de>
2021-01-20 17:24:21 +0100

    Add grid component

---
+++ components/app-grid.stories.js
@@ -0,0 +1,10 @@
+import '../styles/index.css'
+import './app-grid.css'
+import html from './app-grid.html'
+
+export default {
+  title: 'app-grid',
+  component: html,
+}
+
+export const base = () => html
